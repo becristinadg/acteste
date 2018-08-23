1dd28ba1ccee8b91a84858e3cccc83a8a3530396
becristinadg <40190781+becristinadg@users.noreply.github.com>
2018-08-23 17:45:23 -0300

    Inserção do arquivo principal
    iniciando o projeto teste

---
+++ ac linguagem.py
@@ -0,0 +1,39 @@
+def imprimir(dic):
+    print(dic)
+	
+def qntFunc():
+    x = int(input("Insira a quantidade de funcionÃ¡rios a serem cadastrados: "))
+    return x
+	
+def nFuncCPF():
+    info1 = int(input("Insira o CPF do funcionário: "))
+    matri = int(input("Insira o número da matricula do funcionário: "))
+    return info1, matri
+		
+def nFuncNome():
+    info1 = input("Insira o nome do funcionário: ")
+    matri = int(input("Insira o número da matricula do funcionário: "))
+    return info1, matri
+
+def opcao (x):
+    alt = int(input("De qual forma você deseja cadastrar? Digite 1 para CPF e 2 para nome: "))
+    if alt == 1:
+        for i in range(1, x+1):
+            info1, matri = nFuncCPF()
+            func[matri] = info1
+       
+    elif alt == 2:
+        for i in range(1, x+1):
+            info1, matri = nFuncNome()
+            func[matri] = info1
+    return func
+        
+				
+def cadastroFunc():
+    func = {}
+    x = qntFunc()
+    y = opcao(x)
+    imprimir(func)
+
+
+cadastroFunc()
